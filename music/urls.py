faae5340bcfbaa2e019a99407301a9f382244854
Bjoggithk <Bjoggithk@gmail.com>
2018-04-30 10:48:46 +0000

    Fullt af drasti eitt úr detail.html og index

--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,8 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_View(), name='index'),
 
     # /music/712/  (talan er id á albúminu)
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-
-    # /music/<album_id>/<favorite>
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_View(), name='detail'),
 ]
